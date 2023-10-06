fc81cd0fdfb4bb81e789bdb1f445bc2faa2057e0
team-chatgpt <betterworld.hackmania@gmailcom>
2023-10-06 21:26:21 +0530

    final check foR NON LOGGED IN USER

--- main.py
+++ main.py
@@ -5,4 +5,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/causes')
+def causes():
+    return render_template('causes.html')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+@app.route('/event')
+def event():
+    return render_template('event.html')
+
 app.run(debug=True)
